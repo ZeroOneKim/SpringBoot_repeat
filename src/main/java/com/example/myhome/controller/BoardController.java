8b876572c4d2f767b6eebf58798b9afdf71488d7
ZeroOneKim <Wowwow0124@naver.com>
2022-05-29 00:11:24 +0900

    add Paging Event

--- src/main/java/com/example/myhome/controller/BoardController.java
+++ src/main/java/com/example/myhome/controller/BoardController.java
@@ -3,11 +3,12 @@ package com.example.myhome.controller;
 import com.example.myhome.model.Board;
 import com.example.myhome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -16,8 +17,17 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 3) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+        //Page<Board> boards = boardRepository.findAll(pageable);
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = 1;
+        int endPage = boards.getTotalPages();
+        /*int startPage =Math.max(1, boards.getPageable().getPageNumber()-4);*/
+        /*int endPage =Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber()+4);*/
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
